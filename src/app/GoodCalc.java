b55bc2bbb1dacb9a867948678f3dd5adff5abbd9
Dong Ho Kim <imi2080@gmail.com>
2023-05-20 23:05:31 +0900

    This is commit Message!

---
+++ src/app/GoodCalc.java
@@ -0,0 +1,33 @@
+package app;
+import lib.Calculator;
+
+public class GoodCalc extends Calculator{
+
+	
+	public static void main(String[] args) {
+		Calculator c = new GoodCalc();
+		System.out.println(c.add(2, 3));
+		System.out.println(c.substract(2, 3));
+		System.out.println(c.average(new int[] {2,3,4}));
+	}
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public int substract(int a, int b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+	@Override
+	public double average(int[] a) {
+		double sum=0;
+		for(int i: a)
+			sum += i;
+		return sum/a.length;
+	}
+}
